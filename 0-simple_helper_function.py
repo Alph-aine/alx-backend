f2fbe67c601f2bc45e76fa2a3ca18521f9865e45
Abdulbasit <aabdulrafiu721@gmail.com>
2023-11-16 10:45:04 +0100

    moved into correct diriectory

--- 0-simple_helper_function.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-'''Calculate the limit of a page'''
-
-
-def index_range(page: int, page_size: int) -> tuple:
-    '''Returns the start and en indexes of a page'''
-    offset: int = (page - 1) * page_size
-    return (offset, offset + page_size)
