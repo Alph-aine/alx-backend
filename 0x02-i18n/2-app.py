4d13b6bbb45a508935ce5cfc633072e8a1d80cfb
Abdulbasit <aabdulrafiu721@gmail.com>
2023-11-16 10:43:56 +0100

    Gets best language  match for a locale

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+'''Activates babel on the flask app'''
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    '''contains i18n configs'''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    '''Determines the best language match for a locale'''
+    return request.accept_languages.best_match(app.Config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    '''Testing'''
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    """Main function"""
+    app.run(host='0.0.0.0', port=5000)
