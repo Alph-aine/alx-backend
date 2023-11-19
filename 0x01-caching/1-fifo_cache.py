6da2f26efff87568a1d5c5666b62c2c5e6de378d
Abdulbasit <aabdulrafiu721@gmail.com>
2023-11-19 08:51:56 +0100

    Implements a FIFO cache

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    '''Implementing a first in first out cache'''
+    def __init__(self):
+        '''Initializes the FIFO cache'''
+        super().__init__()
+
+    def put(self, key, item):
+        '''puts items to the cache'''
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+        if len(self.cache_data) > self.MAX_ITEMS:
+            first_key = list(self.cache_data.keys())[0]
+            del self.cache_data[first_key]
+            print(f"DISCARD: {first_key}")
+
+    def get(self, key):
+        '''Gets items from the cache'''
+        if key is not None:
+            return self.cache_data.get(key)
